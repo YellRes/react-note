268e2674b907b1cb6564e593206df276e0e5a767
zhangtr <zhangtr@greatld.com>
2024-04-10 18:02:54 +0800

    first init

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import './style.css'
+import SideBar from '@/components/Sidebar'
+
+export default async function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className='container'>
+          <div className='main'>
+            <SideBar />
+
+            <section className='col note-viewer'>
+              { children }
+            </section>
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
